23c6414119416d7c7ecaeaf4516d11312e077e18
Cary Gerber <61208289+cgerber-ns@users.noreply.github.com>
2022-05-04 06:36:19 -0400

    Update main.go

--- main.go
+++ main.go
@@ -10,9 +10,13 @@ var port = flag.Int("p", 8080, "server port")
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/ready", Readied)
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), nil)
 }
 
+func Readied(w http.ResponseWriter, r *http.Request) {
+	w.WriteResponse(http.StatusOK)
+}
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
