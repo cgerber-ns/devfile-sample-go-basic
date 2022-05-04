a41fd71e77314cd735129d62de4b5dfbe59b024a
Cary Gerber <61208289+cgerber-ns@users.noreply.github.com>
2022-05-04 06:41:14 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 }
 
 func Readied(w http.ResponseWriter, r *http.Request) {
-	w.WriteResponse(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 }
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
